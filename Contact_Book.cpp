5c25a005ba7df3688f9dee89a275021bdcf8a37f
Paul Tes <paulasca8462@gmail.com>
2022-03-20 22:36:44 +0300

    Search Contact

--- Contact_Book.cpp
+++ Contact_Book.cpp
@@ -14,7 +14,7 @@ void AddContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE
 void Sort(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex);
 void DisplayContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex);
 void DelletContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex);
-void SearchContact();
+void SearchContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex);
 
 int main () {
     char FullName[SIZE_CONTACT][SIZE_NAME], Email [SIZE_CONTACT][SIZE_EMAIL], CountryCode[SIZE_CONTACT][SIZE_COUNTRY_CODE], PhoneNumber[SIZE_CONTACT][SIZE_PHONE_NUMBER];
@@ -27,7 +27,7 @@ int main () {
         [2] DisplayContact()
         [3] DeleteContact()
         [4] SearchContact()
-        [6] Exit 
+        [5] Exit 
     
     */
     while(operation != 5) {
@@ -41,11 +41,13 @@ int main () {
             break;
         case 3:
             DelletContact(FullName, CountryCode, PhoneNumber, Email, NumberOfContact);
-        // default:
-        //     cerr << "NO operation YOU DEED!";
+            break;
+        case 4: 
+            SearchContact(FullName, CountryCode, PhoneNumber, Email, NumberOfContact);
+            break;
         }
 
-        operation = ContactTable();
+        operation = ContactTable(); // update the input choice.
     }
     if(operation == 5) {
             cout << "Exiting......" << endl;
@@ -54,7 +56,7 @@ int main () {
 }
 
 
-// this is the menue table to chose the user and it returns what the user chose 
+// this is the menue table to chose the user and it returns what the user chose.
 
 int ContactTable() {
     int chose;
@@ -68,7 +70,7 @@ int ContactTable() {
         cout << "[5] Exit Contact" << endl;
         cout << "      >>> ";
         cin >> chose;
-         if (cin) { // if thire is somthing in the buffer check out
+         if (cin) { // if thire is somthing in the buffer check out.
             if (chose < 0 || chose > 5) {
                 cerr << "Envalid Entery Please chose CARFULLY!\n";
             }
@@ -79,12 +81,12 @@ int ContactTable() {
     return chose;
 }
 
-// this is the Add contact operation to add the information in array 
+// this is the Add contact operation to add the information in array.
 // the parameter is constant pointer the array name is the constant in deed the second dimension is must be specifid.
 // any alter of the arry in AddContact the efect is alos in the main function array.
 void AddContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex){ 
     // filling the data.
-    //&Maxindex is the allians of NumberOfContact
+    //&Maxindex is the allians of NumberOfContact.
     cout << endl << "Adding contact....." << endl;
     cout << "Name: [ Pawlos Tesfaye ]";
     cin.ignore(); // clearing the buffer.
@@ -104,30 +106,25 @@ void AddContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE
 void Sort(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex) {
     char TempName[SIZE_NAME], TempCountyCode[SIZE_COUNTRY_CODE], TempPhondNumber[SIZE_PHONE_NUMBER], TempEmail[SIZE_EMAIL];
 
-    for (int i = 0; i < Maxindex; i++) { // Maxindex is the stack since that it is the last number of the contact
-        for (int j = i + 1; j < Maxindex - 1;  j++) {
-            if (FullName[i] != " " && FullName[j] != " ") {
-                if (strcmp(FullName[i], FullName[j]) > 0) { // if FullName[i] is greater than FullName[j] need to swap (case sensetive strcmpi is not working on linux on windos machine you can use it to compute sith out case sensetive).
-                    //swap Name
-                    strcpy(TempName, FullName[j]);
-                    strcpy(FullName[i], FullName[j]);
-                    strcpy(FullName[j], TempName);
-
-                    //swap Country Cod
-                    strcpy(TempCountyCode, CountryCode[i]);
-                    strcpy(CountryCode[i], CountryCode[j]);
-                    strcpy(CountryCode[j], TempCountyCode);
-
-                    //swap number
-                    strcpy(TempPhondNumber, PhoneNumber[i]);
-                    strcpy(PhoneNumber[i], PhoneNumber[j]);
-                    strcpy(PhoneNumber[j], TempPhondNumber);
-
-                    //swap Email
-                    strcpy(TempEmail, Email[i]);
-                    strcpy(Email[i], Email[j]);
-                    strcpy(Email[j], TempEmail);
-                }
+    for (int i = 0; i < Maxindex; i++) { // Maxindex is the stack since that it is the last number of the contact.
+        for (int j = i + 1; j < Maxindex;  j++) {
+            if (strcmp(FullName[i], FullName[j]) > 0) { // if FullName[i] is greater than FullName[j] need to swap (case sensetive strcmpi is not working on linux on windos machine you can use it to compute sith out case sensetive).
+                //swap Name
+                strcpy(TempName, FullName[j]);
+                strcpy(FullName[i], FullName[j]);
+                strcpy(FullName[j], TempName);
+                //swap Country Cod
+                strcpy(TempCountyCode, CountryCode[i]);
+                strcpy(CountryCode[i], CountryCode[j]);
+                strcpy(CountryCode[j], TempCountyCode);
+                //swap number
+                strcpy(TempPhondNumber, PhoneNumber[i]);
+                strcpy(PhoneNumber[i], PhoneNumber[j]);
+                strcpy(PhoneNumber[j], TempPhondNumber);
+                //swap Email
+                strcpy(TempEmail, Email[i]);
+                strcpy(Email[i], Email[j]);
+                strcpy(Email[j], TempEmail);
             }
         }
     }
@@ -138,15 +135,16 @@ void Sort(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], cha
 
 void DisplayContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex){
     char empty[] = "";
-    int count = 0; // counter variable
+    int count = 0; // counter variable.
     //Sort(FullName, CountryCode, PhoneNumber, Email, Maxindex); // always before display we need to sort it.
 
+    //Header of The Contact.
     cout << left << setw(80) << setfill('=') <<  "" << endl << setfill(' ');
     cout << setw(5) << "No" << setw(30) << "Name" << setw(25) << "Phone" << "Email" << endl;
     cout << left << setw(80) << setfill('-') <<  "" << endl << setfill(' ');
 
     for (int i = 0; i < Maxindex; i++) {
-        if(strcmp(FullName[i], empty) != 0) { // if the index is empty it was delleted so no need to display
+        if(strcmp(FullName[i], empty) != 0) { // if the index is empty it was delleted so no need to display.
             count ++;
             cout << left <<setw(5) << count << setw(30) << FullName[i] << setw(5) << CountryCode[i] << setw(19) << PhoneNumber[i] << Email[i]<<endl;
         }
@@ -161,6 +159,8 @@ void DisplayContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_
 void DelletContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex){
     char DeleteName[SIZE_NAME], empty[] = "";
     bool found = false;
+
+    // accepting the name from the user to be Deleted.
     cout << "Enter The name You want to Delete: ";
     cin.ignore();
     cin.getline(DeleteName, SIZE_NAME - 1);
@@ -180,5 +180,34 @@ void DelletContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_C
         cout << DeleteName << " is Successfully DELETED ..... \n";
     }else {
         cout << DeleteName << " is not EXIST in Contact\n";
+    }
+}
+
+// SEARCH CONTACT
+void SearchContact(char FullName[][SIZE_NAME], char CountryCode[][SIZE_COUNTRY_CODE], char PhoneNumber[][SIZE_PHONE_NUMBER], char Email[][SIZE_EMAIL], int &Maxindex){
+    int i, count = 0;
+    // accepting the name from the user which to be searched.
+    bool found = false;
+    char SearchName[SIZE_NAME];
+    cout << "Enter The name You want to Search: ";
+    cin.ignore();
+    cin.getline(SearchName, SIZE_NAME - 1);
+
+
+    //Header of The Contact.
+    cout << left << setw(80) << setfill('=') <<  "" << endl << setfill(' ');
+    cout << setw(5) << "No" << setw(30) << "Name" << setw(25) << "Phone" << "Email" << endl;
+    cout << left << setw(80) << setfill('-') <<  "" << endl << setfill(' ');
+    for (i = 0; i < Maxindex; i++) {
+        if (strcmp(SearchName, FullName[i]) == 0) { // if the name to delete is find in the indexes.
+            count++;
+            found = true;
+            break; // once got the name should be stop the itration.
+        }
+    }
+    if (found) {
+        cout << left <<setw(5) << count << setw(30) << FullName[i] << setw(5) << CountryCode[i] << setw(19) << PhoneNumber[i] << Email[i]<<endl;
+    }else {
+        cout << SearchName << " NO SUCH NAME IN CONTACT\n";
     }
 }
